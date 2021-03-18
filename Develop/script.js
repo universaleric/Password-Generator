0befd17dc67cd6d2b652929bff7d9dcc0f858344
Eric Hay <ericdanielhay@yahoo.com>
2021-03-18 08:24:12 -0500

    Changed initial array for letters to a string and then just used .split() to get array.

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,4 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
 
 function generatePassword() {
   let passLength;
@@ -9,8 +6,10 @@ function generatePassword() {
   let uppercase;
   let numeric;
   let specChar;
-  let alphabetUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  let alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      alphabetUpper = alphabetUpper.split('');
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+      alphabet= alphabet.split('');
   let symbols = ["!","@","#","$","%","&","?"];
   let numbers = [1,2,3,4,5,6,7,8,9,0];
   let setOfChar = [];
